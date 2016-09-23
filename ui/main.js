7646aee22f1ea7ab2be5b5d29d97305c84eec7fe
paulkrisha <paul.krisha@gmail.com>
2016-09-23 09:32:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'This is dedicated to my father and mother';
 var img = document.getElementBId('madi');
 img.onclick=function(){
-    img.style.marginLeft = '100px';
+    img.style.marginLeft = '10px';
 };
