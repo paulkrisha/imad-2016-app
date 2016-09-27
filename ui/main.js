42025fc75e5c35249c8a5deaf98208a8d6695f9d
paulkrisha <paul.krisha@gmail.com>
2016-09-27 10:58:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,19 +12,17 @@
 //};
 
 var button =document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
-  //  var request = new XMLhttpRequest();
-   // request.onreadystatechange = function() {
-   //     if(request.readyState === XMLhttpRequest.DONE){
-    //        if(request.status === 200){
-   //             var counter = request.responseText;
-    counter = counter + 1;
+    var request = new XMLhttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLhttpRequest.DONE){
+          if(request.status === 200){
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-   //         }
-    //    }
-    //};
-    //request.open('GET','http://paulkrisha.imad.hasura-app.io/counter',true);
-    //request.send(null);
+            }
+       }
+   };
+    request.open('GET','http://paulkrisha.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
