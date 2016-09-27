0fbc674edc69cd0a8e9296fd53dcb0bed65504b4
paulkrisha <paul.krisha@gmail.com>
2016-09-27 10:22:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,28 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'This is dedicated to my father and mother';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function() {
-    var interval=setInterval(moveRight,100);
+//console.log('Loaded!');
+//var element = document.getElementById('main-text');
+//element.innerHTML = 'This is dedicated to my father and mother';
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight(){
+  //  marginLeft = marginLeft+10;
+    //img.style.marginLeft=marginLeft+'px';
+//}
+//img.onclick = function() {
+  //  var interval=setInterval(moveRight,100);
+//};
+
+var button =document.getElementById('counter');
+button.onclick = function(){
+    var request = new XMLhttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLhttpRequest.done){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://paulkrisha.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
