27c103d9a18f013f315fc52557e90e1a8739ae47
paulkrisha <paul.krisha@gmail.com>
2016-09-29 14:37:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,6 +142,14 @@ app.get('/counter',function(req,res){
     counter = counter+1;
     res.send(counter.toString());
 });
+
+var names = [];
+app.get('/submit-name', function(req,res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.Stringify(names))
+});
+
 app.get('/:articleName', function(req,res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
