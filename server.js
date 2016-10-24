01aef21a2edf038ff7ac0bb038732321cad54423
paulkrisha <paul.krisha@gmail.com>
2016-10-24 17:44:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var Pool=require('pg').Pool;
+var config={
+    user:'paulkrisha',
+    database:'paulkrisha',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB-PASSWORD
+};
 
 var articles = {
     'article-one' : {
@@ -117,7 +125,7 @@ var htmlTemplate = ` <html>
       <h3>${heading}</h3>
   
         <div>
-            ${date}
+            ${date.todateString()}
         </div>
         <div>
             
@@ -150,9 +158,21 @@ app.get('/submit-name', function(req,res) {
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req,res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req,res) {
+    pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }
+        else {
+            if (result.rows.length===0){
+                res.status(404).send('Article not found');
+            }
+            else {
+                var articleData=result.row[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 
