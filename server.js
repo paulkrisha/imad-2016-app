8000ca1c80f790c17d61a40ddb5b811247ff9197
paulkrisha <paul.krisha@gmail.com>
2016-10-30 14:21:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config);
+/*var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query("SELECT * FROM article",function(err,result){
         if(err){
@@ -66,7 +66,7 @@ app.get('/test-db',function(req,res){
             res.send(JSON.stringify(result.rows));
         }
     });
-});
+});*/
 
 var counter=0;
 app.get('/counter',function(req,res){
