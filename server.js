7bc9cf884d36191a9523a53efab0fc810cb4ddd5
paulkrisha <paul.krisha@gmail.com>
2016-10-30 14:19:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,6 +56,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query("SELECT * FROM article",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter = counter+1;
@@ -69,17 +81,7 @@ app.get('/submit-name', function(req,res) {
     res.send(JSON.stringify(names));
 });
 
-var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query("SELECT * FROM article",function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-});
+
 /*app.get('/articles/:articleName', function(req,res) {
     pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function(err,result){
         if (err){
