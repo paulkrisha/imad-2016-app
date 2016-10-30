d111021b4cc792565cd83df65da7d9ce738142bb
paulkrisha <paul.krisha@gmail.com>
2016-10-30 13:28:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-/*var Pool=require('pg').Pool;
+var Pool=require('pg').Pool;
 var config={
     user:'paulkrisha',
     database:'paulkrisha',
@@ -12,7 +12,7 @@ var config={
     port:'5432',
     password:process.env.DB-PASSWORD
 };
-*/
+
 
 
 function createTemplate(data){
@@ -69,8 +69,18 @@ app.get('/submit-name', function(req,res) {
     res.send(JSON.stringify(names));
 });
 
-/*var pool=new Pool(config);
-app.get('/articles/:articleName', function(req,res) {
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query("SELECT * FROM article",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+/*app.get('/articles/:articleName', function(req,res) {
     pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function(err,result){
         if (err){
             res.status(500).send(err.toString());
