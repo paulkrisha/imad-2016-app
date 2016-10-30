4abdab072d24d9f55d235c68183251b002ceccb4
paulkrisha <paul.krisha@gmail.com>
2016-10-30 15:36:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,8 +92,8 @@ app.get('/articles/:articleName', function(req,res) {
             }
             else {
                 var articleData=result.rows[0];
-                //res.send(createTemplate(articleData)); 
-                res.send(JSON.stringify(articleData));
+                res.send(createTemplate(articleData)); 
+                //res.send(JSON.stringify(articleData));
                 }
         }
     });
