fbc11b0fcb1d39d802a7f594df47da205572b2f7
paulkrisha <paul.krisha@gmail.com>
2016-10-30 15:29:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,10 +91,10 @@ app.get('/articles/:articleName', function(req,res) {
                 res.status(404).send('Article not found');
             }
             else {
-                /*var articleData=result.row[0];
-                res.send(createTemplate(articleData)); */
-                res.send('Article found');
-            }
+                //var articleData=result.row[0];
+                //res.send(createTemplate(articleData)); 
+                res.send(JSON.stringify(result.rows));
+                }
         }
     });
 });
