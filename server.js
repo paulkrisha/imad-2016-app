dd055be4031cca71ea910e63f9843ca1d615abe3
paulkrisha <paul.krisha@gmail.com>
2016-11-12 13:51:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
-var crypto = require('crypto');
-var bodyPparser = require('body-parser');
+/*var crypto = require('crypto');
+var bodyPparser = require('body-parser');*/
 
 var config={
     user:'paulkrisha',
@@ -15,7 +15,7 @@ var config={
 };
 
 app.use(morgan('combined'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 function createTemplate(data){
 var title = data.title;
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input,salt){
+/*function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
@@ -67,7 +67,7 @@ app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'tihis-is-some-random-string');
     res.send(hashedString);
 });
-
+*/
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('select * from test',function(err,result){
