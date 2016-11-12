5962b74b70ab1eeffbe3ee9e02eace48697336e9
paulkrisha <paul.krisha@gmail.com>
2016-11-12 14:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
 var crypto = require('crypto');
-var bodyPparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var config={
     user:'paulkrisha',
