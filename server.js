5391bf57c45445a84785825bd9657e08dff648e9
paulkrisha <paul.krisha@gmail.com>
2016-11-21 13:18:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post('/login',function(req,res){
             var dbString = result.rows[0].password;
             var salt=dbString.split('$')[2];
             var hashedPassword = hash(password,salt);
-            if(hashedPasssword===dbString){
+            if(hashedPassword===dbString){
                 //req.session.auth={userId:result.rows[0].id};
                 res.send('Credentials Correct!');
             }else{
