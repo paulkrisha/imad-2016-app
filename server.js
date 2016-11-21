b760da94dd88bc22ce7186fcc78ee85ea3568440
paulkrisha <paul.krisha@gmail.com>
2016-11-21 09:47:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/create-user',function(req,res){
     var password = req.boby.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password,salt);
-    pool.query('insert into "user" (username,password) values ($1,$2)',[username,password], function(err,result){
+    pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbString], function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else{
