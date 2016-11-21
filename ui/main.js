6d6eb334a7dcbf1eb1ffbf2b541ed43612cf69ef
paulkrisha <paul.krisha@gmail.com>
2016-11-21 13:42:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,14 +58,12 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                alert('Logged in successfully');
-            }else{
-                alert('you are not availabe');
-             if(request.status === 403){
+            }else if(request.status === 403){
                 alert('username/password incorrect');
             }else if (request.status === 500){
                 alert('Something went wrong on the server');
             }
-            }
+            
         }
         
     };
