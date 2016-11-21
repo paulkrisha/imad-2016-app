bb3bb4ae40ff88d7809d85eef9b5f1eac9ee2f08
paulkrisha <paul.krisha@gmail.com>
2016-11-21 10:36:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post('/create-user',function(req,res){
 
 app.post('/login',function(req,res){
     var username=req.body.username;
-    var password = req.boby.password;
+    var password = req.body.password;
     pool.query('select * from "user" where username = $1',[username], function(err,result){
         if(err){
             res.status(500).send(err.toString());
