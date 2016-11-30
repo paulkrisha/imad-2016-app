6ad97bf893dc09ff11c79824dd9aef14d1f5c46d
paulkrisha <paul.krisha@gmail.com>
2016-11-30 17:23:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -131,7 +131,7 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                console.log(this.responsetext);
+                console.log(this.responseText);            
                 loadLoggedInUser(this.responseText);
             } else {
                 loadLoginForm();
